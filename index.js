42bcee4f2cebaec0c393d6d5e7c169999a375112
aminaouini <aminaouini565@gmail.com>
2020-02-12 23:37:47 +0100

    deploying

--- index.js
+++ index.js
@@ -14,13 +14,13 @@ app.use(cors());
 require('./routes/userRoutes')(app);
 require('./routes/messagesRoutes')(app);
 require('./routes/postsRoutes')(app);
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname + '/client/build/index.html'));
+});
 
 if (process.env.NODE_ENV === 'production') {
     const path = require('path');
     app.use(express.static('client/build'));
-    app.get('*', (req, res) => {
-        res.sendFile(path.join(__dirname + '/client/build/index.html'));
-    });
 
     app.get('*', (req, res) => {
         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
