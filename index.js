4afcaf34c020653dd9fbef2866bbc4b4dd0e7572
aminaouini <aminaouini565@gmail.com>
2020-02-13 00:12:21 +0100

    deploying

--- index.js
+++ index.js
@@ -14,12 +14,10 @@ app.use(cors());
 require('./routes/userRoutes')(app);
 require('./routes/messagesRoutes')(app);
 require('./routes/postsRoutes')(app);
-if (require('path')) {
-    app.get('*', (req, res) => {
-        res.sendFile(require('path').join(__dirname + '/client/build/index.html'));
-    });
 
-}
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname + '/client/build/index.html'));
+});
 
 if (process.env.NODE_ENV === 'production') {
     const path = require('path');
