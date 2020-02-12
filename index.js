3102277e7ac241ae86fab82bf467d134d10ada1f
aminaouini <aminaouini565@gmail.com>
2020-02-12 18:54:17 +0100

    test4

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const express = require('express');
 const app = express();
 const cors = require('cors');
@@ -11,20 +12,19 @@ require('dotenv').config();
 
 app.use(cors());
 
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname + '/client/build/index.html'));
+});
 require('./routes/userRoutes')(app);
 require('./routes/messagesRoutes')(app);
 require('./routes/postsRoutes')(app);
 
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static('client/build'));
-    app.get('*', (req, res) => {
-        res.sendFile(path.join(__dirname + '/client/build/index.html'));
-    });
 
-    // const path = require('path');
-    // app.get('*', (req, res) => {
-    //     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
-    // })
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+    })
 
 }
 
