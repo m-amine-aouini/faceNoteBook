497b0abf45861addc1b4079f322feba5149ee507
aminaouini <aminaouini565@gmail.com>
2020-02-14 11:53:46 +0100

    connect with database on heroku

--- db/DB.js
+++ db/DB.js
@@ -1,9 +1,9 @@
 const { Pool, Client } = require('pg');
 const pool = new Pool({
-    user: 'amin',
-    host: 'localhost',
-    database: 'faceNoteBook',
-    password: '123456789',
+    user: 'smnabegfpvxexs',
+    host: 'ec2-54-195-247-108.eu-west-1.compute.amazonaws.com',
+    database: 'd8ft64aa899lss',
+    password: '4426143161bc76d529475d4d98c62fb8da525bb4952c00e685746e00e28a85d2',
     port: 5432,
 });
 
