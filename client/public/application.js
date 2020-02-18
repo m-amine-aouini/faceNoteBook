9ee3a03a37605ecca7f25fe94f266a0759f1a8eb
aminaouini <aminaouini565@gmail.com>
2020-02-18 13:40:49 +0100

    functional socket

--- client/public/application.js
+++ client/public/application.js
@@ -3,7 +3,7 @@ $(function () {
     // let socket = io.connect('localhost:3001')
 
     socket.on('message', (data) => {
-        console.log('message')
+
         $('#' + data.receiver).prepend('<div><h6>' + data.sender + '</h6><p>' + data.message + '</p></div>')
         $('#' + data.sender).prepend('<div><h6>' + data.sender + '</h6><p>' + data.message + '</p></div>')
     })
