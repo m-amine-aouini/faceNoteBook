37e8010cc9ffef64feca9776398bc22868a493fc
aminaouini <aminaouini565@gmail.com>
2020-02-18 13:13:07 +0100

    testing socket1

--- client/public/application.js
+++ client/public/application.js
@@ -1,5 +1,5 @@
 $(function () {
-    let socket = io.connect('postgres://smnabegfpvxexs:4426143161bc76d529475d4d98c62fb8da525bb4952c00e685746e00e28a85d2@ec2-54-195-247-108.eu-west-1.compute.amazonaws.com:5432/d8ft64aa899lss');
+    let socket = io.connect('ec2-54-195-247-108.eu-west-1.compute.amazonaws.com:5432/');
 
     socket.on('message', (data) => {
         console.log('message')
