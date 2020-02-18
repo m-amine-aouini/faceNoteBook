68e5b79b9c0a4f55ace45753954cd628a19181f7
aminaouini <aminaouini565@gmail.com>
2020-02-18 11:17:22 +0100

    make socket on heroku4

--- client/public/application.js
+++ client/public/application.js
@@ -2,6 +2,7 @@ $(function () {
     let socket = io.connect('https://ec2-54-195-247-108.eu-west-1.compute.amazonaws.com:5432');
 
     socket.on('message', (data) => {
+        console.log('message')
         $('#' + data.receiver).prepend('<div><h6>' + data.sender + '</h6><p>' + data.message + '</p></div>')
         $('#' + data.sender).prepend('<div><h6>' + data.sender + '</h6><p>' + data.message + '</p></div>')
     })
